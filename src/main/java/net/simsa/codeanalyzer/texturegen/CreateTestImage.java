bfc55dec22a68c9138a25500226d0514cc21fbdd
Jenny Brown <jennybrown8@gmail.com>
2015-09-19 20:53:49 -0500

    JMagick JNI is uncooperative on Mac due to versioning mismatches

---
+++ src/main/java/net/simsa/codeanalyzer/texturegen/CreateTestImage.java
@@ -0,0 +1,9 @@
+package net.simsa.codeanalyzer.texturegen;
+
+public class CreateTestImage {
+
+    public static void main(String[] args) throws Exception {
+	System.setProperty("java.awt.headless", "true");
+    }
+
+}
