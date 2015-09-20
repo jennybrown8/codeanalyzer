e9747b49755fd98d31eb0ca94b317b36caafac70
Jenny Brown <jennybrown8@gmail.com>
2015-09-20 01:14:02 -0500

    Contact sheet for debugging

---
+++ scripts/contact_sheet.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Creates a contact sheet for about 2300 images. No real reason just a nice thumbnail view.
+# Grid layout pins both width and height so there's no fixing the gaps of white space between.
+
+montage -verbose -font Arial -pointsize 10 -background "#FFFFFF" -fill "gray" -define png:size=200x80 -geometry "200x80" -tile "40x60" -auto-orient ../target/images/*.png ~/contact.png
+
