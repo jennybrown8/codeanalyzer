f84762dd1ac7ac32cd08476165f21952323a0c8f
Jenny Brown <jennybrown8@gmail.com>
2015-09-20 21:57:20 -0500

    Size adjusted now that pngs have method names listed

--- scripts/contact_sheet.sh
+++ scripts/contact_sheet.sh
@@ -3,5 +3,5 @@
 # Creates a contact sheet for about 2300 images. No real reason just a nice thumbnail view.
 # Grid layout pins both width and height so there's no fixing the gaps of white space between.
 
-montage -verbose -font Arial -pointsize 10 -background "#FFFFFF" -fill "gray" -define png:size=200x80 -geometry "200x80" -tile "40x60" -auto-orient ../target/images/*.png ~/contact.png
+montage -verbose -font Arial -pointsize 10 -background "#FFFFFF" -fill "gray" -define png:size=300x300 -geometry "300x300" -tile "40x60" -auto-orient ../target/images/*.png ~/contact.png
 
