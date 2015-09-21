b137a4b427b82ad3d2b3e2e4ef5d6373d9a44f38
Jenny Brown <jennybrown8@gmail.com>
2015-09-20 19:48:12 -0500

    Preparing for adding methods

--- src/main/java/com/codeforanyone/codeanalyzer/analyzers/ClassEntityVisitor.java
+++ src/main/java/com/codeforanyone/codeanalyzer/analyzers/ClassEntityVisitor.java
@@ -1,7 +1,5 @@
 package com.codeforanyone.codeanalyzer.analyzers;
 
-import net.java.truevfs.access.TFile;
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.objectweb.asm.AnnotationVisitor;
@@ -13,8 +11,13 @@ import org.objectweb.asm.Opcodes;
 
 import com.codeforanyone.codeanalyzer.model.JClass;
 
-public class ClassEntityVisitor extends ClassVisitor {
+import net.java.truevfs.access.TFile;
 
+public class ClassEntityVisitor extends ClassVisitor  {
+
+    static int classesVisited = 0;
+    static final int DEBUG_ABORT_AFTER_N_CLASSES=30;
+    
     Logger log = LogManager.getLogger();
     
     JClass jclass;
@@ -23,6 +26,10 @@ public class ClassEntityVisitor extends ClassVisitor {
     public ClassEntityVisitor() {
 	super(Opcodes.ASM4);
 	jclass = new JClass();
+	classesVisited++;
+	if (classesVisited > DEBUG_ABORT_AFTER_N_CLASSES) {
+	    throw new RuntimeException("Debug threshold reached, stopping.");
+	}
     }
     
     public void setFile(TFile file) {
@@ -60,7 +67,7 @@ public class ClassEntityVisitor extends ClassVisitor {
     }
 
     public void visitAttribute(Attribute attr) {
-//	log.debug("   Attribute " + attr.toString());
+	log.debug("   Attribute " + attr.toString());
     }
 
     public void visitInnerClass(String name, String outerName, String innerName, int access) {
@@ -68,13 +75,26 @@ public class ClassEntityVisitor extends ClassVisitor {
     }
 
     public FieldVisitor visitField(int access, String name, String desc, String signature, Object value) {
-//	log.debug("   Field " + desc + " " + name + " " + signature + " " + value);
+	// Field Ljava/lang/String; name null null
+	// Field J serialVersionUID null 1
+	// Field I value null null
+	// Field Z immutable null null
+	// Field Ljava/lang/Class; clazz Ljava/lang/Class<*>; null
+	// Field Lorg/apache/wicket/util/visit/IVisitFilter; ANY null null
+	// Field Lorg/apache/wicket/util/visit/Visit$Action; CONTINUE_BUT_DONT_GO_DEEPER null null
+	// Field [Lorg/apache/wicket/util/visit/Visit$Action; $VALUES null null
+	// Field Lorg/apache/wicket/util/time/Time; lastModifiedTime null null
+	log.debug("   Field " + desc + " " + name + " " + signature + " " + value);
 	return null;
     }
 
     public MethodVisitor visitMethod(int access, String name, String desc, String signature,
 	    String[] exceptions) {
-//	log.debug("   Method " + name + desc);
+	// Method <init> (Ljava/lang/String;)V
+	// Method locateInputSource ()Lorg/xml/sax/InputSource;
+	// Method <init> (Ljava/lang/String;Lorg/apache/wicket/util/xml/CustomEntityResolver$1;)V
+	// Method access$200 (Lorg/apache/wicket/util/xml/CustomEntityResolver$EntityKey;)Ljava/lang/String;
+	log.debug("   Method " + name + " " + desc);
 	return null;
     }
 
